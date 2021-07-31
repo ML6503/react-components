65a185d47e54a853ea3e5abc33305396d214edd2
ML6503 <ml6503@gmail.com>
2021-07-31 15:56:32 +0300

    init: add simple react-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,13 @@
-import React from "react";
-import "./app.css";
+import React from 'react';
+import './app.css';
 import Search from './search';
+import Images from './images';
 
-const App = () => {
-  
-    return(
-      <Search />
-    );
-  };
-
+const App = (): JSX.Element => (
+  <div className="search-page">
+    <Search />
+    <Images />
+  </div>
+);
 
 export default App;
