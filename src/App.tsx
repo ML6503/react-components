2232d707c23d3c2634d68ba207bed0108c428109
ML6503 <ml6503@gmail.com>
2021-08-14 21:02:59 +0300

    feat add css transition

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './app.css';
 import NewsPage from './components/newsPage';
 import About from './components/about';
 import Navigation from './components/navigation';
+import NoMatch from './components/noMatch';
+import { CSSTransition } from 'react-transition-group';
+
+
+const routes = [
+  { path: '/', name: 'Home', Component: NewsPage },
+  { path: '/about', name: 'About', Component: About },
+  // { path: '*', name: 'NoMatch', Component: NoMatch },
+]
 
 const App = (): JSX.Element => (
     <Router>
       <div>
           <Navigation />
-          <Switch>
-              <Route exact path="/">
-                <NewsPage />
-              </Route>
-              <Route path="/about">
-                <About />
-              </Route>
-          </Switch>
+          
+          <main className="container">
+          {routes.map(({ path, Component }) => (
+            <Route key={path} exact path={path}>
+              {({ match }) => (
+                <CSSTransition
+                  in={match != null}
+                  timeout={300}
+                  classNames="page"
+                  unmountOnExit
+                >
+                  <div className="container">
+                    <Component />
+                  </div>
+                </CSSTransition>
+              )}
+            </Route>
+          ))}
+      </main>
       </div>
     </Router>
     
---
+++ src/components/noMatch.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import '../app.css';
+
+const NoMatch = () => {
+    return (
+      <h1>Not found 404</h1>
+    )
+};
+
+export default NoMatch;
